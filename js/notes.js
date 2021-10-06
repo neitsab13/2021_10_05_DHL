2be70620ce92a7a8092fbed71a9b18707244d34d
bastien.haldy <bastien.haldy@cbainfo.fr>
2021-10-06 16:12:51 +0200

    rien compris

--- js/notes.js
+++ js/notes.js
@@ -1,42 +1,41 @@
-/**
- * 
- * @param {*} exp // expediteur
- * @param {*} destinataire // destinataire
- * @param {*} desc // descriptif
- * @param {*} dateCrea // date création 
- * @param {*} dateExe // date publication
- * @param {*} priority // priorité
- * @param {*} title // titre
- */
+var Note = function () {
+    this.destinataire = '';
+    this.expediteur = '';
+    this.priorite = 'LOW';
+    this.dateCrea = new Date;
+    this.dateCible = new Date;
+    this.description = '';
+    this.titre = '';
 
-function Note(exp1,destinataire1,desc1,dateCrea1,dateExe1,priority1,title1){
-    var exp=exp1,destinataire=destinataire1,desc=desc1,dateCrea=dateCrea1,dateExe=dateExe1,priority=priority1,title=title1;
-    /**
-     * mise a jour titre
-     * @param {string} titre 
-     */
-    function updateTitre(titre){
-        title=titre;
-    }
-    this.setTitle=updateTitre;
-    /**
-     * 
-     * @param {string} description 
-     */
-    this.setPriority=function(description){
-        desc=description
-    }
-    this.setThis=function(objetNoteFromJson){
-
-    }
-    this.getexpediteur=function(){
-        return exp;
-    }
-    this.toJson=function(){
-        return '{"expediteur":"'+exp+'","priorite":"'+priority+'","desc":"'+desc+'","titre":"'+title+'","dest":"'+destinataire+'"}'    
-    }
 }
-var note=new Note()
 
-Note.prototype.maValeur=1234;
+var Notes = function () {
+    var _liste = [];
+    this.push = function (note) {
+        // checker si le param est bien une note de classe
+        if (note instanceof Note) {
+            return _liste.push(note);
+        }
+        else {
+            return _liste.length;
+        }
+    }
+    this.find = function (titre) {
+        return _liste.find(function (elementdeliste) {
+            return elementdeliste.titre === titre
+        });
 
+    }
+    this.indexliste = function (titre) {
+        return _liste.findindex(function (elementdeliste) {
+            return elementdeliste.titre === titre
+        })
+    }
+    this.get = function (index) {
+        return _liste[index];
+    }
+
+
+
+}
+var notes = new Notes
